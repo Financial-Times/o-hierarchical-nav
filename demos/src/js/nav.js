af21bdcc50f76c50ad91d0f378a2daafc4f17bb5
Gabrielle <gabivk@gmail.com>
2018-01-17 12:44:39 +0000

    some demo js fixes

--- demos/src/js/nav.js
+++ demos/src/js/nav.js
@@ -1,6 +1,6 @@
 /*global require*/
 
-const Nav = require('../../src/js/Nav');
+const Nav = require('../../../src/js/Nav');
 const navEls = document.querySelectorAll('.o-hierarchical-nav');
 
 for (let c = 0, l = navEls.length; c < l; c++) {
